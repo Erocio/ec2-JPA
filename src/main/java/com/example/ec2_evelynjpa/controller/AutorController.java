17c6058f84db26567b8faf3c474a700cb875e8a8
Juan <Juan@DESKTOP-434PQRF>
2021-11-04 12:35:15 -0500

    version2

---
+++ src/main/java/com/example/ec2_evelynjpa/controller/AutorController.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.ec2_evelynjpa.controller;
+
+import org.springframework.stereotype.Controller;
+
+
+
+/**
+ *
+ * @author Juan
+ */
+@Controller
+public class AutorController {
+}
